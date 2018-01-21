10585c6b9b9cc8ba9b696a1b73f308117105d647
mushood <mushood.badulla@gmail.com>
2018-01-21 19:51:11 +0400

    Initial Commit

---
+++ src/firstServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace mushood\first;
+
+use Illuminate\Support\ServiceProvider;
+
+class firstServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
